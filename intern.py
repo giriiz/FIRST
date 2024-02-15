478213416db0425f9a269474f1e9791db6d00ac4
giriiz <150990715+giriiz@users.noreply.github.com>
2024-02-15 15:06:13 +0530

    Update intern.py

--- intern.py
+++ intern.py
@@ -1,1 +1,9 @@
-internship
+pip install librosa
+import librosa
+
+audio_file_path = 'your_audio_file.wav'
+
+audio_data, sampling_rate = librosa.load(audio_file_path, sr=None)
+
+print(f"The sampling rate of the audio file is: {sampling_rate} Hz")
+
